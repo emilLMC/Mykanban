6cb461e1cb4045941c158b08e3890d78011a736c
unknown <emilgeorgethomas95@gmail.com>
2021-07-21 18:07:56 +0530

    updating errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './task/task';
+import { CdkDragDrop, DragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,21 @@ export class AppComponent {
     {title:'Buy Milk',description:'Go to store and buy Milk'},
     {title:'Buy Bread',description:'Go to store and buy Bread'}
   ];
+  inProgress: Task[]=[];
+  done: Task[]=[];
+  drop(event: CdkDragDrop<Task[]>): void{
+    if(event.previousContainer===event.container){
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+
+  }
+  edit(list : string, task: Task): void{
+    
+  }
 }
